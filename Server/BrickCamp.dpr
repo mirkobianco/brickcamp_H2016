071becdf49e24325c33dd79d55cdaf22ffc337ad
Mirko Bianco <m.bianco@coolblue.nl>
2016-10-05 10:24:49 +0200

    Model files refactoring

--- Server/BrickCamp.dpr
+++ Server/BrickCamp.dpr
@@ -8,14 +8,14 @@ uses
   BrickCamp.IDB in 'Interfaces\BrickCamp.IDB.pas',
   BrickCamp.services in 'BrickCamp.services.pas',
   BrickCamp.Model.TEmployee in '..\Model\Classes\BrickCamp.Model.TEmployee.pas',
-  BrickCamp.Repositories.IEmployee in 'Interfaces\BrickCamp.Repositories.IEmployee.pas',
   BrickCamp.Resources.TEmployee in 'Classes\BrickCamp.Resources.TEmployee.pas',
   BrickCamp.TDB in 'Classes\BrickCamp.TDB.pas',
   BrickCamp.TService in 'Classes\BrickCamp.TService.pas',
   BrickCamp.TSettings in 'Classes\BrickCamp.TSettings.pas',
-  BrickCamp.Repositories.TEmployee in 'Classes\BrickCamp.Repositories.TEmployee.pas',
   BrickCamp.Resources.IEmployee in 'Interfaces\BrickCamp.Resources.IEmployee.pas',
-  BrickCamp.Model.IEmployee in '..\Model\Interfaces\BrickCamp.Model.IEmployee.pas';
+  BrickCamp.Model.IEmployee in '..\Model\Interfaces\BrickCamp.Model.IEmployee.pas',
+  BrickCamp.Repositories.TEmployee in '..\Model\Classes\BrickCamp.Repositories.TEmployee.pas',
+  BrickCamp.Repositories.IEmployee in '..\Model\Interfaces\BrickCamp.Repositories.IEmployee.pas';
 
 {$R *.RES}
 
